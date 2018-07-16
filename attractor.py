0c6f2b1aa0dc69a67f9d686c117b46cbcde6e092
marxcar <shannat@symmesdarcy.com>
2018-07-16 12:03:04 -0400

    Fixed the attractor function and graphing

--- attractor.py
+++ attractor.py
@@ -14,8 +14,9 @@ dt = 0.01
 stepCnt = 5000
 
 
-def attractor(s=10,r=28,b=2.667):
+def attractor(param=[10,28,2.667]):
 	# Need one more for the initial values
+	s, r, b = param
 	xs = np.empty((stepCnt + 1,))
 	ys = np.empty((stepCnt + 1,))
 	zs = np.empty((stepCnt + 1,))
@@ -30,67 +31,32 @@ def attractor(s=10,r=28,b=2.667):
 	    xs[i + 1] = xs[i] + (x_dot * dt)
 	    ys[i + 1] = ys[i] + (y_dot * dt)
 	    zs[i + 1] = zs[i] + (z_dot * dt)
-	return xs, zs
+	return xs, ys
 
 
-#ax = fig.gca(projection='3d')
-'''
-plt.subplot(121)
-plt.title("b=2.667")
-plt.plot(x_vals, y_vals#, zs, lw=0.5
-)
-plt.subplot(122)
-plt.title("b=2")
-plt.plot(attractor(b=2)[0], attractor(b=2)[1])
-'''
-num_graphs = 4
+num_graphs = 10
 
 s_range = [5,20]
 r_range = [10,50]
 b_range = [1,3]
+parameters = ['s', 'r', 'b']
 param_ranges = [s_range, r_range, b_range]
 count = 0	
 for param_range in param_ranges:
-	print(1 + count*num_graphs)
-	for index, param_val in zip(range(1 + count*num_graphs ,(count+2)*num_graphs),np.linspace(param_range[0],param_range[1],num_graphs)):
-		
-		print("3" + str(num_graphs) + "" + str(index))
+	for index, param_val in zip(range(1 + count*num_graphs, (count+2)*num_graphs),np.linspace(param_range[0],param_range[1],num_graphs)):
+
+		param_array = [10,28,2.667]
+		param_array[count] = param_val
 		plt.subplot(3,num_graphs,index)
 
-		plt.title("b=%f" % (param_val))
-		xs, ys = attractor(b=param_val)
+		plt.title("%s=%.2f" % (parameters[count],param_val))
+		plt.axis('off')
+		xs, ys = attractor(param_array)
 		plt.plot(xs,ys)
+	
 	count += 1
 
+plt.savefig("X&Y.png")
 
-
-"""
-
-for index, b_val in zip(range(1,num_graphs+1),np.d linspace(b_range[0],b_range[1],num_graphs)):
-	plt.subplot(num_graphs,3,index)
-	plt.title("b=%f" % (b_val))
-	xs, ys = attractor(b=b_val)
-	plt.plot(xs,ys)
-	print(str(num_graphs) + "3" + str(index))
-
-for index, s_val in zip(range(num_graphs+1,(num_graphs*2)+1),np.linspace(s_range[0],s_range[1],num_graphs)):
-	plt.subplot(num_graphs,3,index)
-	print(str(num_graphs) + "3" + str(index))
-	plt.title("s=%f" % (s_val))
-	xs, ys = attractor(s=s_val)
-	plt.plot(xs,ys)
-for index, r_val in zip(range(num_graphs+4,(num_graphs*3)+1),np.linspace(r_range[0],r_range[1],num_graphs)):
-	plt.subplot(num_graphs,3,index)
-	print(str(num_graphs) + "3" + str(index))
-	plt.title("r=%f" % (r_val))
-	xs, ys = attractor(r=r_val)
-	plt.plot(xs,ys)
-
-"""
-#plt.savefig("thirdshit.png")
-#plt.set_xlabel("X Axis")
-#plt.set_ylabel("Y Axis")
-#ax.set_zlabel("Z Axis")
 #plt.set_title("Lorenz Attractor")
-
 plt.show()
